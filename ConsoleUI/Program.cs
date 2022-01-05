90313850e952e27db1811374e7853c80da8ef86d
OkhanT <m.okhan.tanriverdi@hotmail.com>
2022-01-06 02:11:21 +0300

    06.01.2022 - MyFinalProject

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,10 +10,35 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //ProductTest();
+            //IoC 
+            //CategoryTest();
+
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetailDtos())
+            {
+                Console.WriteLine(product.ProductName + " // " + product.CategoryName);
+            }
+
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
 
-            foreach (var product in productManager.GetByUnitsPrice(50,100))
+            foreach (var product in productManager.GetByUnitsPrice(50, 100))
             {
                 Console.WriteLine(product.ProductName);
             }
